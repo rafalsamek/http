c4f25f335a6b5d86b5afb1dacd33c9532b41f83a
rsamek <rafal.samek@gmail.com>
2024-03-10 11:31:34 +0100

    @RequestParam with list

--- src/main/java/com/example/http/RequestParamController.java
+++ src/main/java/com/example/http/RequestParamController.java
@@ -1,8 +1,11 @@
 package com.example.http;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RequestParamController {
@@ -10,5 +13,15 @@ public class RequestParamController {
     @GetMapping("/param")
     public String concat(@RequestParam String left, @RequestParam(required = false, defaultValue = "") String right) {
         return left.concat(right);
+    }
+
+    @GetMapping("/param/add")
+    public Integer add(@RequestParam List<Integer> numbers) {
+        int result = 0;
+        for (Integer number : numbers) {
+            result += number;
+        }
+
+        return result;
     }
 }
