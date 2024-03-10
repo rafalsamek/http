1902f8cd425b5d15d80afd424a965d9c01fd5f41
rsamek <rafal.samek@gmail.com>
2024-03-10 12:40:40 +0100

    @RequestBody with objects

--- src/main/java/com/example/http/BodyController.java
+++ src/main/java/com/example/http/BodyController.java
@@ -1,8 +1,35 @@
 package com.example.http;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+class User {
+    private String name;
+    private String surname;
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
 
 @RestController
 public class BodyController {
@@ -10,5 +37,15 @@ public class BodyController {
     @PutMapping("/body/lower")
     public String lower(@RequestBody String text) {
         return text.toLowerCase();
+    }
+
+    @PostMapping("/body/save")
+    public String save(@RequestBody User user) {
+        return user.getName() + " " + user.getSurname() + " saved!";
+    }
+
+    @PutMapping("/body/save")
+    public User put(@RequestBody User user) {
+        return user;
     }
 }
