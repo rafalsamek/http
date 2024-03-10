119cc85ed1bdb06a8bddef9418d93d23ff93c89a
rsamek <rafal.samek@gmail.com>
2024-03-10 16:23:21 +0100

    ErrorPlayground

--- src/main/java/com/example/http/ErrorPlayground.java
+++ src/main/java/com/example/http/ErrorPlayground.java
@@ -1,11 +1,16 @@
 package com.example.http;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ErrorPlayground {
     @GetMapping("/error/1")
     public ResponseEntity<String> test1() {
-        return ResponseEntity.ok("Test");
+//        return ResponseEntity.ok("Test");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Princess is in another castle");
     }
 }
